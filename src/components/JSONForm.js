18c19a593b99e8faaea730b3e4b99fdf41698de6
Chaitanya Baranwal <chaitanyabaranwal@gmail.com>
2019-01-06 20:36:12 +0530

    Change indentation to 2 in App.js

--- src/components/JSONForm.js
+++ src/components/JSONForm.js
@@ -4,37 +4,37 @@ import { Typography } from '@material-ui/core';
 
 // Form to handle JSON Input by users
 class JSONForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {value: this.props.json};
+  constructor(props) {
+    super(props);
+    this.state = {value: this.props.json};
 
-        this.handleChange = this.handleChange.bind(this);
-    }
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-    handleChange(event) {
-        this.setState({ value: event.target.value });
-    }
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
 
-    render() {
-        return (
-            <div>
-                <Typography align='center' color='inherit' variant='display1'>
-                    JSON Visualizer
-                </Typography>
-                <div className='justified'>
-                    <form onSubmit={() => this.props.handleSubmit(this.state.value)}>
-                        <textarea type='text' onChange={this.handleChange} value={this.state.value} rows='20' cols='80'>
-                            {this.props.json}
-                        </textarea>
-                        <br />
-                        <Button variant='contained' color='primary' type='submit'>
-                            Submit JSON
-                        </Button>
-                    </form>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Typography align='center' color='inherit' variant='display1'>
+            JSON Visualizer
+        </Typography>
+        <div className='justified'>
+          <form onSubmit={() => this.props.handleSubmit(this.state.value)}>
+            <textarea type='text' onChange={this.handleChange} value={this.state.value} rows='20' cols='80'>
+              {this.props.json}
+            </textarea>
+            <br />
+            <Button variant='contained' color='primary' type='submit'>
+              Submit JSON
+            </Button>
+          </form>
+        </div>
+      </div>
+    )
+  }
 
 }
 
